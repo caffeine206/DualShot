585ee024928918d5570e588c350de2c0d6e726d8
John Louie <gotacoolemail@yahoo.com>
2014-05-07 00:59:53 -0700

    Cleaned up the particle prototype. Old folder had a lot of junk files.

--- Particle_Prototype/Assets/Resources/Scripts/GlobalBehavior.cs
+++ Particle_Prototype/Assets/Resources/Scripts/GlobalBehavior.cs
@@ -6,9 +6,7 @@ public class GlobalBehavior : MonoBehaviour {
 	private GameObject respawnParticle;
 	Ship theShip;
 	ParticleSystem theFlames;
-	//GameObject theShield;
-
-	// Use this for initialization
+	
 	void Start () {
 		if (respawnParticle == null) {
 			respawnParticle = Resources.Load("Prefabs/Respawn") as GameObject;
@@ -16,10 +14,8 @@ public class GlobalBehavior : MonoBehaviour {
 
 		theShip = GameObject.Find("Ship").GetComponent<Ship>();
 		theFlames = GameObject.Find("ShipFlames").GetComponent<ParticleSystem>();
-		//theShield = GameObject.Find("Shield").GetComponent<>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		StartCoroutine(respawn());
 	}
@@ -32,7 +28,6 @@ public class GlobalBehavior : MonoBehaviour {
 			yield return new WaitForSeconds(1f);
 			theShip.gameObject.SetActive(true);
 			theFlames.particleSystem.Clear();
-			//theShield.
 		}
 	}
 }
--- Particle_Prototype/Assets/Resources/Scripts/Ship.cs
+++ Particle_Prototype/Assets/Resources/Scripts/Ship.cs
@@ -10,26 +10,14 @@ using System.Collections;
 
 public class Ship : MonoBehaviour {
 
-	private const float Speed = 20f;
+	private const float Speed = 30f;
 	private const float RotateSpeed = 120f;
 	private const float FireRate = 1f;
 	private float LastTimeFired = -1f;
-	public int eggsOnScreen = 0;
-	private float worldHeight;
-	private float worldWidth;
-	//The hero is a cube! Height and width are equal.
-	private float heroWidth;
-	//private ParticleSystem charge = null;
 	private bool isCharging = false;
 	private GameObject explosion;
 
 	void Start () {
-		/*
-		worldHeight = Camera.main.orthographicSize;
-		worldWidth = worldHeight * Camera.main.aspect;
-		heroWidth = transform.lossyScale.x;
-		*/
-		
 		if (explosion == null) {
 			explosion = Resources.Load("Prefabs/SmallExplosion") as GameObject;
 		}
@@ -40,14 +28,9 @@ public class Ship : MonoBehaviour {
 		orbBlaster();
 		StartCoroutine("charging");
 		selfDestruct();
-		//collision();
-		//fireProjectile();
 	}
 
-	//The hero's movement, moves 20 units/second
 	private void movement() {
-		//GetAxisRaw removes the speed up and slow down of the hero's movement,
-		//makes movement more precise
 		transform.position += Input.GetAxisRaw("Vertical") * transform.up *
 			(Speed * Time.smoothDeltaTime);
 		transform.Rotate(Vector3.forward, -1f * Input.GetAxis("Horizontal") * (RotateSpeed * Time.smoothDeltaTime));
@@ -88,58 +71,4 @@ public class Ship : MonoBehaviour {
 			gameObject.SetActive(false);
 		}
 	}
-
-	/*
-	private void createCharge() {
-		if (Input.GetButtonDown("Fire1")) {
-			if (isCharging == false) {
-				isCharging = true;
-				ParticleSystem c = Instantiate(charge) as ParticleSystem;
-				StartCoroutine(charging(c));
-			}
-		} else if (Input.GetButtonUp("Fire1")) {
-			isCharging = false;
-		}
-	}
-
-	IEnumerator charging(ParticleSystem theCharge) {
-		theCharge.startSize = 1f;
-		yield return new WaitForSeconds(5f);
-		theCharge.startSize = 2f;
-		yield return new WaitForSeconds(5f);
-		theCharge.startSize = 3f;
-	}
-	*/
-	/*
-	private void collision() {
-		if (transform.position.y > worldHeight - heroWidth / 2) {
-			transform.position -= new Vector3(0f, transform.position.y - (worldHeight - heroWidth / 2), 0f);
-		}
-		
-		if (transform.position.y < -(worldHeight - heroWidth / 2)) {
-			transform.position -= new Vector3(0f, transform.position.y + (worldHeight - heroWidth / 2), 0f);
-		}
-		
-		if (transform.position.x > worldWidth - heroWidth / 2) {
-			transform.position -= new Vector3(transform.position.x - (worldWidth - heroWidth / 2), 0f);
-		}
-
-		if (transform.position.x < -(worldWidth - heroWidth / 2)) {
-			transform.position -= new Vector3(transform.position.x + (worldWidth - heroWidth / 2), 0f);
-		}
-	}
-
-	private void fireProjectile() {
-		if (Input.GetAxisRaw("Fire1") > 0f && (Time.realtimeSinceStartup - LastTimeFired) > FireRate) {
-			GameObject p = Instantiate(projectile) as GameObject;
-			Projectile proj = p.GetComponent<Projectile>();
-			if (proj != null) {
-				p.transform.position = transform.position;
-				proj.SetForwardDirection(transform.up);
-			}
-			eggsOnScreen++;
-			LastTimeFired = Time.realtimeSinceStartup;
-		}
-	}
-	*/
 }
